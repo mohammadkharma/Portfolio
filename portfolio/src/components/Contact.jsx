edd4ad6eab3472aed3a83c894fa08e1b7a48e15f
MYK <mohammad.kh.8688@gmail.com>
2020-08-30 16:06:39 +0200

    initialized Contact page

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -3,7 +3,25 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="contact">
-      <h3>Contact</h3>
+      <form action="POST" data-netlify="true">
+        <div className="field">
+          <input type="text" name="name" id="name" placeholder="Name" />
+        </div>
+        <div className="field">
+          <input type="email" name="email" id="email" placeholder="Email" />
+        </div>
+        <div className="field">
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+          ></textarea>
+        </div>
+        <div data-netlifyrecaptcha="true"></div>
+        <div className="field">
+          <input className="submit" type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
   );
 };
