746da138f8fe1863f96b6f15f6ce77da08fb58e4
MYK <mohammad.kh.8688@gmail.com>
2020-09-04 23:29:20 +0200

    Updates

---
+++ precache-manifest.dd1474c8e1225d36e1146a6b33d0c10f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec754c3d17cbec17f322f77a68a19fe",
+    "url": "/Portfolio/portfolio/index.html"
+  },
+  {
+    "revision": "9a2b0771c528b63c294e",
+    "url": "/Portfolio/portfolio/static/css/main.302faa38.chunk.css"
+  },
+  {
+    "revision": "854a18045f3b3ff37738",
+    "url": "/Portfolio/portfolio/static/js/2.1515793e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/portfolio/static/js/2.1515793e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a2b0771c528b63c294e",
+    "url": "/Portfolio/portfolio/static/js/main.af80754a.chunk.js"
+  },
+  {
+    "revision": "1f252655e4f40646fe86",
+    "url": "/Portfolio/portfolio/static/js/runtime-main.937846c1.js"
+  }
+]);
